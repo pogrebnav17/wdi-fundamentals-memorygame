ecac036246d832c6378eeb28842ff85e2dd1eb50
Nika Pogrebna <pogrebnav17@mail.wlu.edu>
2017-07-20 23:42:26 -0400

    Added functionality for cards to flip

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+// add reset button
+// keep track and display the users score 
+
 console.log("Up and running!");
 var cards = [
   {
@@ -26,22 +29,33 @@ var cards = [
 var cardsInPlay = [];
 
 var checkForMatch = function(){
-  if (cardsInPlay.length === 2){
     if (cardsInPlay[0] === cardsInPlay[1]) {
-      console.log("You found a match!");
+      alert("You found a match!");
     } else {
-    console.log("Sorry, try again.");
+    alert("Sorry, try again.");
     }
-  }
 }
 
-var flipCard = function(cardId){
+var flipCard = function(){
+  var cardId = this.getAttribute('data-id');
   console.log("User flipped " + cards[cardId].rank);
   console.log(cards[cardId].cardImage);
   console.log(cards[cardId].suit);
   cardsInPlay.push(cards[cardId].rank);
-  checkForMatch();
+  this.setAttribute('src', cards[cardId].cardImage);
+  if (cardsInPlay.length === 2){ //should show both cards flipped before message
+    checkForMatch();
+  }
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function () {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+  }
+}
+
+createBoard();
